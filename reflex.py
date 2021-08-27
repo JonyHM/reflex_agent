2444eac6dd5b48a957a6d2d8054aa1c03adf5381
guianderson <guilherme.anderson34@outlook.com>
2021-08-26 22:30:10 -0300

    Conexão com Banco mysql, tutorial de como criar ambiente virtual, requirements.txt

--- reflex.py
+++ reflex.py
@@ -1,7 +1,23 @@
+import pymysql.cursors
+
 ## TODO: 
-# - Conexão com o MariaDB; 
 # - Gerenciamento de regras (criar, excluir e visualizar regras)
 # - Menu para escolher entre gerenciamento e validação de regras.
+
+## DONE:
+# - Conexão com o MariaDB; 
+
+
+con = pymysql.connect(host='localhost',
+                              user='root',
+                              password='',
+                              database='mysql',
+                              cursorclass=pymysql.cursors.DictCursor)
+
+with con.cursor() as cursor:
+    cursor.execute("SELECT * FROM USER")
+    print(cursor.fetchall())
+
 
 dbRule = [
   {
