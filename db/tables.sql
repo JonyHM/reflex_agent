6fa848e2ee0bdde953fcef4f27c88ac34d5c46aa
Jonathas H. Moraes <jonathas.moraes@fatec.sp.gov.br>
2021-08-31 22:14:22 -0300

    Adding DB connection to base methodss

--- db/tables.sql
+++ db/tables.sql
@@ -1,35 +1,36 @@
 #############################################################
 
 # CRIANDO TABELA RULES
-CREATE TABLE rules(
+CREATE TABLE IF NOT EXISTS rules(
 	id_rules int(9) AUTO_INCREMENT,
 	relation varchar(50) NOT NULL,
 	action_rules varchar(50) NOT NULL,
 	PRIMARY KEY (id_rules)
-)
+);
 
 #############################################################
 
 # CRIANDO TABELA PRODUCT
-CREATE TABLE product(
+CREATE TABLE IF NOT EXISTS product(
 	id_product int(9) AUTO_INCREMENT,
 	product_name varchar(30) NOT NULL,
 	id_rules int(9) NOT NULL,
 	PRIMARY KEY (id_product)
-)
+);
 
 #############################################################
 
 # CRIANDO CHAVE EXTRANGEIRA 
 ALTER TABLE reflex.product 
-ADD CONSTRAINT id_rules_FK FOREIGN KEY (id_rules) 
-REFERENCES reflex.rules(id_rules) 
-ON DELETE CASCADE 
-ON UPDATE CASCADE;
+  ADD CONSTRAINT id_rules_FK FOREIGN KEY (id_rules) 
+  REFERENCES reflex.rules(id_rules) 
+  ON DELETE CASCADE 
+  ON UPDATE CASCADE;
 
 #############################################################
 
-# CRIANDO FUN플O PARA INSERIR RULE
+# CRIANDO FUNCAO PARA INSERIR RULE
+DELIMITER //
 CREATE OR REPLACE FUNCTION FNC_INSERT_RULE (fnc_relation varchar(50), fnc_action_rules varchar(50))
 RETURNS int
 LANGUAGE SQL
@@ -37,20 +38,26 @@ NOT DETERMINISTIC
 BEGIN
 
   DECLARE next_id_rule int;
-  SELECT max(id_rules + 1) INTO next_id_rule FROM rules;
+  SELECT `AUTO_INCREMENT` INTO next_id_rule
+    FROM  INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_SCHEMA = 'reflex'
+    AND TABLE_NAME   = 'rules';
   insert into rules (relation, action_rules) values (fnc_relation, fnc_action_rules);
   RETURN next_id_rule;
 
-END
+END; //
+
+DELIMITER ;
 
 #############################################################
 
-# CHAMADA DA FUN플O
-select FNC_INSERT_RULE('<=', 'teste');
+# CHAMADA DA FUNCAO
+select FNC_INSERT_RULE('==', 'leite');
 
 #############################################################
 
-# CRIANDO FUN플O PARA INSERIR product
+# CRIANDO FUNCAO PARA INSERIR product
+DELIMITER //
 CREATE OR REPLACE FUNCTION FNC_INSERT_PRODUCT (fnc_product_name varchar(30), fnc_id_rules int(9))
 RETURNS int
 LANGUAGE SQL
@@ -62,31 +69,14 @@ BEGIN
   insert into product (product_name, id_rules) values (fnc_product_name, fnc_id_rules);
   RETURN next_id_product;
 
-END
+END; //
+
+DELIMITER ;
 
 #############################################################
 
-# CHAMADA DA FUN플O
+# CHAMADA DA FUNCAO
 
-select FNC_INSERT_PRODUCT('batata', 1);
+select FNC_INSERT_PRODUCT('p찾o', 1);
 
 #############################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- reflex.py
+++ reflex.py
@@ -1,61 +1,66 @@
 from unidecode import unidecode
 import pymysql.cursors
 
-## TODO:
-# - possibilidade de mais de uma percepção
+# TODO:
+# - Mudar nomes dos elementos da regra
+# - arrumar listagem
+# - arrumar retorno do id na funç~~ao sql
 
-con = pymysql.connect(host='localhost',
-                      port=3309,
-                      user='root',
-                      password='',
-                      database='reflex',
-                      cursorclass=pymysql.cursors.DictCursor)
+con = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='fatec',
+    database='reflex',
+    cursorclass=pymysql.cursors.DictCursor
+)
 
-with con.cursor() as cursor:
-    cursor.execute("INSERT INTO rules (pecepts, relation, action_res) values ('teste', '>', 'banana')")
-    con.commit()
-    con.close()
-    print(cursor.fetchall())
-
-dbRules = [
-    {
-        'pecepts': [
-            {
-                'percept': 'pao',
-                'relation': '=='
-            },
-            {
-                'percept': 'intolerancia',
-                'relation': '!='
-            }
-        ],
-        'action': 'leite'
-    },
-    {
-        'percept': '1',
-        'relation': '==',
-        'action': 'dois'
-    }
-]
+cursor = con.cursor()
+percepts = []
+dbRules = []
+# [
+#     {
+#         'percepts': [
+#             'cafe'
+#         ],
+#         'relation': '==',
+#         'action': 'leite'
+#     },
+#     {
+#         'products': [
+#             'cafe',
+#             'leite'
+#         ],
+#         'relation': '==',
+#         'action': 'pao'
+#     }
+# ]
 
 
 ######## Eval engine #########
 
-def evalRule(rule, percept):
-    percept = unidecode(percept)
+def evalRule(rule, percepts):
     relation = rule['relation']
-    rulePercept = rule['percept']
-    evaluation = True
+    action = rule['action']
+    evaluation = False
+    ruleEval = []
 
     if len(rule['percepts']) > 1:
-        for percept in rule['percepts']:
-            evaluation = eval(f'percept {relation} rulePercept') and evaluation
+        for rulePercept in rule['percepts']:
+            for inputPercept in percepts:
+                evaluation = eval(f'inputPercept {relation} rulePercept')
+                if evaluation:
+                    break
+            ruleEval.append(evaluation)
 
-        if evaluation:
-            return rule['action']
+        if len(ruleEval) == len(rule['percepts']) and all(ruleEval) and action not in percepts:
+            return action
 
-    elif eval(f'percept {relation} rulePercept'):
-        return rule['action']
+    else:
+        for inputPercept in percepts:
+            rulePercept = rule['percepts'][0]
+            evaluation = eval(f'inputPercept {relation} rulePercept')
+            if evaluation and action not in percepts:
+                return action
 
     return None
 
@@ -69,13 +74,23 @@ def actionEngine(dbRule, percept):
     return actions
 
 
-def startShoppingCart():
-    percept = input('Informe o item da sua lista de compras: ')
-    ## ask for a new item or eval
-    print(actionEngine(dbRules, percept.lower()))
+def addItemToCard(title='Informe o novo item da sua lista de compras: '):
+    percept = input(title)
+    percepts.append(unidecode(percept))
 
+
+def startShoppingCart():
+    # check dbRules and only proceed if > 0
+    addItemToCard()
+    shoppingCartMenu()
+
+
+def evaluateRules():
+    print(actionEngine(dbRules, percepts))
+    finalMenu()
 
 ######### Helpers #########
+
 
 def getNumberInput(options, inputTitle='Selecione a opção do menu: '):
     inputValue = input(inputTitle)
@@ -90,8 +105,70 @@ def getNumberInput(options, inputTitle='Selecione a opção do menu: '):
 
     return getNumberInput(options, inputTitle)
 
+def exitProject():
+    con.close()
+    exit()
+
+def parse_sql(filename):
+    data = open(filename, 'r').readlines()
+    stmts = []
+    DELIMITER = ';'
+    stmt = ''
+
+    for lineno, line in enumerate(data):
+        if not line.strip():
+            continue
+
+        if line.startswith('--'):
+            continue
+
+        if 'DELIMITER' in line:
+            DELIMITER = line.split()[1]
+            continue
+
+        if (DELIMITER not in line):
+            stmt += line.replace(DELIMITER, ';')
+            continue
+
+        if stmt:
+            stmt += line
+            stmts.append(stmt.strip())
+            stmt = ''
+        else:
+            stmts.append(line.strip())
+    return stmts
+
+
+######### DB #########
+
+def createRuleAndProducts(rule):
+    products = rule['products']
+    relation = rule['relation']
+    action = rule['action']
+
+    ruleFunc = f"SELECT FNC_INSERT_RULE('{relation}', '{action}') as id"
+    cursor.execute(ruleFunc)
+    result = cursor.fetchone()
+    con.commit()
+    ruleId = 1
+
+    if result['id'] != None:
+        ruleId = result['id']
+
+    for product in products:
+        productFunc = f"SELECT FNC_INSERT_PRODUCT('{product}', {ruleId})"
+        cursor.execute(productFunc)
+    
+    con.commit()
+
+def deleteRuleFromDB(ruleId):
+    deleteFunc = f"DELETE FROM rules WHERE id_rules = {ruleId};"
+    cursor.execute(deleteFunc)
+    con.commit()
+
 
 ######### Menus #########
+
 
 def returnToMain(previousMethod):
     print('\n---------------------------------------------\n')
@@ -105,7 +182,38 @@ def returnToMain(previousMethod):
     options = {
         1: previousMethod,
         2: showMainMenu,
-        3: exit
+        3: exitProject
+    }
+
+    menuInput = getNumberInput(options)
+    options[menuInput]()
+
+
+def finalMenu():
+    print('\n---------------------------------------------\n')
+
+    print('\t1 - Voltar ao menu principal')
+    print('\t2 - Sair')
+
+    print('\n---------------------------------------------\n')
+
+    options = {
+        1: showMainMenu,
+        2: exit
+    }
+
+    menuInput = getNumberInput(options)
+    options[menuInput]()
+
+
+def shoppingCartMenu():
+    print('Gostaria de adicionar um novo item na lista?')
+    print('\t1 - Sim')
+    print('\t2 - Não\n')
+
+    options = {
+        1: startShoppingCart,
+        2: evaluateRules
     }
 
     menuInput = getNumberInput(options)
@@ -129,7 +237,7 @@ def showManagementMenu():
         2: deleteSelection,
         3: listRules,
         4: showMainMenu,
-        5: exit
+        5: exitProject
     }
 
     menuInput = getNumberInput(options)
@@ -149,7 +257,7 @@ def showMainMenu():
     options = {
         1: showManagementMenu,
         2: startShoppingCart,
-        3: exit
+        3: exitProject
     }
 
     menuInput = getNumberInput(options)
@@ -168,9 +276,9 @@ def showRules(returnToMainMenu=False):
         opperator = rule['relation']
         action = rule['action']
         print(f'\n{index + 1} -')
-        print(f'Percepção: {perception}')
+        print(f'Produtos: {perception}')
         print(f'Relação: {opperator}')
-        print(f'Ação: {action}\n')
+        print(f'Recomendação: {action}\n')
 
     if returnToMainMenu:
         returnToMain(lambda: showRules(returnToMainMenu))
@@ -180,23 +288,40 @@ def createNewRule():
     print('\nCriar nova regra\n')
 
     rule = {}
+    products = []
+    product = input('Produto: ')
+    products.append(product)
+    response = None
 
-    rule['percept'] = input('Percepção: ')
-    rule['relation'] = input('Relação: ')
-    rule['action'] = input('Ação: ')
+    while response.lower() != 'n' or response.lower() != 's' or response != '':
+        response = input(f'Adicionar mais um produto à regra? [S/n] ')
 
-    dbRules.append(rule)
+        if response.lower() == 'n':
+            break
+        elif response.lower() == 's' or response == '':
+            product = input('Produto: ')
+            products.append(product)
+
+        else:
+            print('Opção inválida! Insira S para sim ou N para não\n')
+
+    rule['products'] = products
+    rule['relation'] = '=='
+    rule['action'] = input('Recomendação: ')
+
+    createRuleAndProducts(rule)
+    
     print('\nRegra criada com sucesso!\n')
     returnToMain(createNewRule)
 
 
 def deleteRule(ruleId):
-    response = input(f'Excluír regra {ruleId}? [S/n]')
+    response = input(f'Excluír regra {ruleId}? [S/n] ')
 
     if response.lower() == 'n':
         return
-    elif response.lower() != 's' or response != '':
-        dbRules.pop(ruleId - 1)
+    elif response.lower() == 's' or response == '':
+        deleteRuleFromDB(ruleId)
         print(f'\nRegra {ruleId} excluída com sucesso!')
     else:
         print('Opção inválida! Insira S para sim ou N para não\n')
@@ -205,7 +330,8 @@ def deleteRule(ruleId):
 
 def deleteSelection():
     showRules()
-    ruleId = getNumberInput(dbRules, 'Insira o índice da regra a ser excluída (0 para voltar): ')
+    ruleId = getNumberInput(
+        dbRules, 'Insira o índice da regra a ser excluída (0 para voltar): ')
 
     if ruleId == 0:
         showManagementMenu()
@@ -216,4 +342,22 @@ def deleteSelection():
 
 ######### Initialization #########
 
+# stmts = parse_sql('db\\tables.sql')
+# for line in stmts:
+#     print(line)
+#     cursor.execute(line)
+
+# rules = cursor.fetchall()
+# rule = rules[0]
+# print(rule['action_rules'])
+
 showMainMenu()
+
+# rule = {}
+# rule['products'] = ['café', 'açúcar', 'queijo', 'leite']
+# rule['relation'] = '=='
+# rule['action'] = 'pão'
+
+# createRuleAndProducts(rule)
+# ruleId = input('id: ')
+# deleteRuleFromDB(int(ruleId))
