0ee07545f06712077056e02d4a39b9df94c982b1
guianderson <guilherme.anderson34@outlook.com>
2021-08-31 21:07:37 -0300

    creating dictionary with database's information

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 #############################################################
 
 # CRIANDO TABELA RULES
-CREATE TABLE rules(
+CREATE TABLE IF NOT EXISTS rules(
 	id_rules int(9) AUTO_INCREMENT,
 	relation varchar(50) NOT NULL,
 	action_rules varchar(50) NOT NULL,
@@ -11,25 +11,26 @@ CREATE TABLE rules(
 #############################################################
 
 # CRIANDO TABELA PRODUCT
-CREATE TABLE product(
+CREATE table IF NOT EXISTS product(
 	id_product int(9) AUTO_INCREMENT,
 	product_name varchar(30) NOT NULL,
 	id_rules int(9) NOT NULL,
 	PRIMARY KEY (id_product)
-)
+);
+
 
 #############################################################
 
-# CRIANDO CHAVE EXTRANGEIRA 
-ALTER TABLE reflex.product 
-ADD CONSTRAINT id_rules_FK FOREIGN KEY (id_rules) 
-REFERENCES reflex.rules(id_rules) 
-ON DELETE CASCADE 
+# CRIANDO CHAVE EXTRANGEIRA
+ALTER TABLE reflex.product
+ADD CONSTRAINT id_rules_FK FOREIGN KEY (id_rules)
+REFERENCES reflex.rules(id_rules)
+ON DELETE CASCADE
 ON UPDATE CASCADE;
 
 #############################################################
 
-# CRIANDO FUNÇÃO PARA INSERIR RULE
+# CRIANDO FUNÃ‡ÃƒO PARA INSERIR RULE
 CREATE OR REPLACE FUNCTION FNC_INSERT_RULE (fnc_relation varchar(50), fnc_action_rules varchar(50))
 RETURNS int
 LANGUAGE SQL
@@ -41,16 +42,16 @@ BEGIN
   insert into rules (relation, action_rules) values (fnc_relation, fnc_action_rules);
   RETURN next_id_rule;
 
-END
+end;
 
 #############################################################
 
-# CHAMADA DA FUNÇÃO
+# CHAMADA DA FUNÃ‡ÃƒO
 select FNC_INSERT_RULE('<=', 'teste');
 
 #############################################################
 
-# CRIANDO FUNÇÃO PARA INSERIR product
+# CRIANDO FUNÃ‡ÃƒO PARA INSERIR product
 CREATE OR REPLACE FUNCTION FNC_INSERT_PRODUCT (fnc_product_name varchar(30), fnc_id_rules int(9))
 RETURNS int
 LANGUAGE SQL
@@ -62,31 +63,26 @@ BEGIN
   insert into product (product_name, id_rules) values (fnc_product_name, fnc_id_rules);
   RETURN next_id_product;
 
-END
+end;
 
 #############################################################
 
-# CHAMADA DA FUNÇÃO
+# CHAMADA DA FUNÃ‡ÃƒO
 
-select FNC_INSERT_PRODUCT('batata', 1);
+select FNC_INSERT_PRODUCT('teste3', 2);
 
 #############################################################
 
+create or replace view all_percepts as
+select p.id_rules,
+	   p.product_name "percept",
+	   r.relation "relation",
+	   r.action_rules "action"
+	   from product p
+inner join rules r on p.id_rules = r.id_rules
 
+#############################################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+select * from rules r
+select * from product p
+select * from all_percepts
